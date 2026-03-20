36043b7d452d7e1b8a79e63414f20642960027ae
jaidenhenley <jaidenhenley@gmail.com>
2026-03-19 21:11:05 -0400

    [Feature] added a new Lake class with properties lakeName, lakeDepth, and lakeID, conforming to Codable.

---
+++ MichiganAPIWeather/Lake.swift
@@ -0,0 +1,14 @@
+//
+//  Lake.swift
+//  MichiganAPIWeather
+//
+//  Created by Jaiden Henley on 3/19/26.
+//
+
+import Foundation
+
+class Lake: Codable {
+    let lakeName: String
+    let lakeDepth: Int
+    let lakeID: UUID
+}

2477b4bd481995f79890848e707b949c21615653
jaidenhenley <jaidenhenley@gmail.com>
2026-04-24 14:53:01 -0400

    [Clean] Renamed and refactored test files
    - Renamed `BeachScoringServiceTests.swift` to `BeachScoringTests.swift`
    - Removed unused `BeachCrowdClassifierTests` and its associated files
    - Updated import statements and test structure to match new file names and contents

---
+++ MichiganAPIWeatherTests/BeachScoringTests.swift
@@ -0,0 +1,16 @@
+//
+//  BeachScoringTests.swift
+//  MichiganAPIWeatherTests
+//
+//  Created by Jaiden Henley on 4/24/26.
+//
+
+import Testing
+
+struct BeachScoringTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
